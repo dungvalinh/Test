4d239da98e1633bf4f81ebc688b1f573b58b1875
DUNG <dung1398st@gmail.com>
2022-06-29 19:38:55 +0700

    BT luy thua

--- Program.cs
+++ Program.cs
@@ -3,17 +3,15 @@ namespace VietDung{
     class Program{
         static void Main(string[] args)
         {     
-            int n = int.Parse(Console.ReadLine());
-            int answer =0 ;
-            for (int i = 1; i <=n ; i +=2) {
-                answer += i ;
-                    }  
-                    Console.WriteLine(answer);
-    
-           
-            
-         
-                
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int answer =1;
+            while(b>0)
+                {
+                    answer *=a;
+                    b--; 
+                }
+               Console.Write(answer);
         }
             
         }
