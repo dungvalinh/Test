5770d14d0b0e6422b5bdef5c015670f67bc0206b
DUNG <dung1398st@gmail.com>
2022-06-29 19:22:48 +0700

    BT 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace VietDung{
+    class Program{
+        static void Main(string[] args)
+        {     
+            int n = int.Parse(Console.ReadLine());
+            int answer =0 ;
+            for (int i = 1; i <=n ; i +=2) {
+                answer += i ;
+                    }  
+                    Console.WriteLine(answer);
+    
+           
+            
+         
+                
+        }
+            
+        }
+}
+
